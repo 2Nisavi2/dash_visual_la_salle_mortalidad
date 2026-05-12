93e1cc8e03e552774714172e4c128fc8e723206c
2Nisavi2 <avila220895@gmail.com>
2026-05-11 21:05:23 -0500

    Modulos de Tratamientos de Datos

---
+++ modulo_data/diccionarios.py
@@ -0,0 +1,92 @@
+## Espacio de almacenamiento de diccionarios de variables categóricas
+
+## Área de Defunción
+dicc_area_defuncion = {1: 'Cabecera municipal',
+                       2: 'Centro poblado',
+                       3: 'Rural disperso',
+                       9: 'Sin información'}
+
+## Sitio defunción
+dicc_sitio_defuncion = {1: 'Hospital/clínica',
+                        2: 'Centro/puesto de salud',
+                        3: 'Casa/domicilio',
+                        4: 'Lugar de trabajo',
+                        5: 'Via pública',
+                        6: 'Otro',
+                        9: 'Sin información'}
+
+## Meses
+dicc_meses = {1: 'Enero',
+              2: 'Febrero',
+              3: 'Marzo',
+              4: 'Abril',
+              5: 'Mayo',
+              6: 'Junio',
+              7: 'Julio',
+              8: 'Agosto',
+              9: 'Septiembre',
+              10: 'Octubre',
+              11: 'Noviembre',
+              12: 'Diciembre'}
+
+## Sexo
+dicc_sexo = {1: 'Masculino',
+             2: 'Femenino',
+             3: 'Indeterminado'}
+
+## Estado Civil
+dicc_estado_civil = {1: 'No estaba casado(a) y llevaba dos o más años viviendo con su pareja',
+                     2: 'No estaba casado(a) y llevaba menos de dos años viviendo con su pareja',
+                     3: 'Estaba separado(a), divorciado(a)',
+                     4: 'Estaba viudo(a)',
+                     5: 'Estaba soltero(a)',
+                     6: 'Estaba casado(a)',
+                     9: 'Sin información'}
+
+## Grupo de Edad
+dicc_grupo_edad = {'00': 'Menor de una hora',
+                    '01': 'Menor de un día',
+                    '02': 'De 1 a 6 días',
+                    '03': 'De 7 a 27 días',
+                    '04': 'De 28 a 29 días',
+                    '05': 'De 1 a 5 meses',
+                    '06': 'De 6 a 11 meses',
+                    '07': 'De 1 año',
+                    '08': 'De 2 a 4 años',
+                    '09': 'De 5 a 9 años',
+                    '10': 'De 10 a 14 años',
+                    '11': 'De 15 a 19 años',
+                    '12': 'De 20 a 24 años',
+                    '13': 'De 25 a 29 años',
+                    '14': 'De 30 a 34 años',
+                    '15': 'De 35 a 39 años',
+                    '16': 'De 40 a 44 años',
+                    '17': 'De 45 a 49 años',
+                    '18': 'De 50 a 54 años',
+                    '19': 'De 55 a 59 años',
+                    '20': 'De 60 a 64 años',
+                    '21': 'De 65 a 69 años',
+                    '22': 'De 70 a 74 años',
+                    '23': 'De 75 a 79 años',
+                    '24': 'De 80 a 84 años',
+                    '25': 'De 85 a 89 años',
+                    '26': 'De 90 a 94 años',
+                    '27': 'De 95 a 99 años',
+                    '28': 'De 100 años y más',
+                    '29': 'Edad desconocida'}
+
+## Nivel Educativo
+dicc_nivel_educativo = {1: 'Preescolar',
+                         2: 'Básica primaria',
+                         3: 'Básica secundaria',
+                         4: 'Media académica o clásica',
+                         5: 'Media técnica',
+                         6: 'Normalista',
+                         7: 'Técnica profesional',
+                         8: 'Tecnológica',
+                         9: 'Profesional',
+                         10: 'Especialización',
+                         11: 'Maestría',
+                         12: 'Doctorado',
+                         13: 'Ninguno',
+                         99: 'Sin información'}
---
+++ modulo_data/tratamiento_data.py
@@ -0,0 +1,93 @@
+## Función para el tratameinto incial de datos desde capa bronce a capa
+def transformacion_data(data_anexo_1_bronce,
+                        data_anexo_2,
+                        data_divi):
+    """
+    Función para el tratameinto incial de datos desde capa bronce a capa.    
+    Parámetros:    -----------
+    df : pandas.DataFrame de las tres fuentes bronce (anexo_1, anexo_2 y divipola)   
+    Retorna:    --------
+    resumen_df : pandas.DataFrame de las tres fuentes tratadas.
+    """   
+    ## Liberias
+    import unicodedata
+
+    ## Data Anexo 1----------------------------------------------------------------
+    data_anex_1_pla = data_anexo_1_bronce.copy()
+
+    ## Normalización de nombres de columnas
+    data_anex_1_pla.columns = data_anex_1.columns.str.lower()
+    data_anex_1_pla
+
+    ## renombre de columnas
+    data_anex_1_pla.rename(columns={
+        "año": "anio",
+        "grupo_edad1": "grupo_edad_1",
+        "idprofesional": "id_profesional"
+    }, inplace=True)
+
+    ## Asignación de valores categóricos
+    from modulo_data.diccionarios import (
+        dicc_area_defuncion,
+        dicc_estado_civil,
+        dicc_sexo,
+        dicc_grupo_edad,
+        dicc_meses,
+        dicc_nivel_educativo,
+        dicc_sitio_defuncion
+    )
+
+    # Diccionario maestro: columna -> diccionario de reemplazo
+    diccionarios = {
+        'area_defuncion': dicc_area_defuncion,
+        'estado_civil': dicc_estado_civil,
+        'sexo': dicc_sexo,
+        'grupo_edad_1': dicc_grupo_edad,
+        'mes': dicc_meses,
+        'nivel_educativo': dicc_nivel_educativo,
+        'sitio_defuncion': dicc_sitio_defuncion
+    }
+
+    # Aplicación
+    for columna, diccionario in diccionarios.items():
+        
+        data_anex_1_pla[columna] = (
+            data_anex_1_pla[columna]
+            .map(diccionario)
+            .fillna(data_anex_1_pla[columna])
+        )
+
+    ## Codigos muerte incompletos
+    data_anex_1_pla['cod_muerte'] = data_anex_1_pla['cod_muerte'].astype(str)
+    data_anex_1_pla['cod_muerte'] = data_anex_1_pla['cod_muerte'].apply(
+        lambda x: x + 'X' if len(x) == 3 else x
+    )
+
+    data_anex_1_pla = data_anex_1_pla.drop(columns = ['cod_departamento', 'cod_municipio'])
+
+    ## Data Anexo 2----------------------------------------------------------------
+    data_anex_2_pla = data_anexo_2.copy()
+
+    ## Eliminación de signos de puntuación
+    data_anex_2_pla.columns = [
+        unicodedata.normalize('NFKD', col).encode('ascii', errors='ignore').decode('utf-8')
+        for col in data_anex_2_pla.columns
+    ]
+
+    ## Linea al piso para espacios
+    data_anex_2_pla.columns = data_anex_2_pla.columns.str.replace(' ', '_') 
+    data_anex_2_pla
+
+    ## Reemplazo de nombres
+    data_anex_2_pla.rename(columns={
+        "codigo_de_la_cie-10_tres_caracteres": "codigo_cie10_tres_caracteres",
+        "descripcion__de_codigos_mortalidad_a_tres_caracteres": "descripcion_codigo_mortalidad_tres_caracteres",
+        "codigo_de_la_cie-10_cuatro_caracteres": "codigo_cie10_cuatro_caracteres",
+        "descripcion__de_codigos_mortalidad_a_cuatro_caracteres": "descripcion_codigo_mortalidad_cuatro_caracteres"
+    }, inplace=True)
+
+    ## Data Divipola----------------------------------------------------------------
+    data_divi_pla = data_divi.copy()
+    data_divi_pla.columns = data_divi.columns.str.lower()
+
+    return data_anex_1_pla, data_anex_2_pla, data_divi_pla
